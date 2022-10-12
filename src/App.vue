3fa320f6873ced7e89429ab2278e9dc9d7cd50f0
Fernando Herrera <fernando.herrera85@gmail.com>
2022-10-12 11:26:09 -0400

    counter setup

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+import { ref } from 'vue';
+const counter = ref<number>(0);
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>Counter: {{ counter }}</h1>
+    <button @click="counter++">+1</button>
+  </div>
+</template>
+
+<style scoped>
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
